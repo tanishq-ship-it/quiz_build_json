587d473ed24d3528557bdca1b27dc34b0276d9dc
TanishqTapAcademy <tanishq@scaleswift.com>
2025-12-21 23:15:28 +0530

    public view

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import NotFound from "./pages/NotFound";
 import Preview from "./pages/preview";
 import PreviewPlay from "./pages/PreviewPlay";
 import Editorial from "./pages/Editorial";
+import PublicQuiz from "./pages/PublicQuiz";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<QuizCreator />} />
+      <Route path="/:quizId" element={<PublicQuiz />} />
       <Route path="/preview" element={<Preview />} />
       <Route path="/preview/:quizId" element={<Preview />} />
       <Route path="/preview-play/:quizId" element={<PreviewPlay />} />
